63bf0a09ccd1d1c24a312a10d6168df247eb3f27
Jayant Kumar Sharma <jayant.sharma_cs22@gla.ac.in>
2025-08-17 20:14:26 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,28 @@
+const cityInput=document.querySelector('.city-input')
+const searchBtn=document.querySelector('.search-btn')
+const apikey='2581de31fbee39b044dcb79b59565dd8'
+searchBtn.addEventListener('click',()=>{
+    if(cityInput.value.trim()!=''){
+       updateWeatherInfo(cityInput.value)
+        cityInput.value=''
+        cityInput.blur()
+    }
+})
+cityInput.addEventListener('keydown',(event)=>{
+    if(event.key=='Enter' &&
+        cityInput.value.trim()!=''
+    ){
+        updateWeatherInfo(cityInput.value)
+        cityInput.value=''
+        cityInput.blur()
+    }
+})
+async function getFetchData(endPoint, city){
+ const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apikey}&units=metric`
+ const response = await fetch(apiUrl)
+ return response.json()
+}
+async function updateWeatherInfo(city){
+    const WeatherData=await getFetchData('weather',city)
+    console.log(WeatherData)
+}
